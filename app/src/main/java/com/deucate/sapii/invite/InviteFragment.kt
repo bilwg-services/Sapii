341be028203eff1cf3a89b9d9cad3017a6ceeec2
KartikPatelOfficial <patelkartik1910@gmail.com>
2018-12-06 07:36:16 +0530

    Add: Invite Fragment

--- app/src/main/java/com/deucate/sapii/invite/InviteFragment.kt
+++ app/src/main/java/com/deucate/sapii/invite/InviteFragment.kt
@@ -3,17 +3,32 @@ package com.deucate.sapii.invite
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.deucate.sapii.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_invite.view.*
 
 class InviteFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        startActivity(Intent(activity!!, InviteActivity::class.java))
-        return inflater.inflate(R.layout.fragment_invite, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_invite, container, false)
+
+        rootView.inviteID.text = SpannableStringBuilder(FirebaseAuth.getInstance().uid)
+
+        rootView.inviteButton.setOnClickListener {
+            val i = Intent(Intent.ACTION_SEND)
+            i.type = "text/plain"
+            i.putExtra(Intent.EXTRA_SUBJECT, "Sapii")
+            var sAux = "\nLet me recommend you this application\n\n"
+            sAux += "https://play.google.com/store/apps/details?id=com.deucate.sapii \n\n"
+            i.putExtra(Intent.EXTRA_TEXT, sAux)
+            startActivity(Intent.createChooser(i, "choose one"))
+        }
+        return rootView
     }
 
 }
